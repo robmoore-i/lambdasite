d38c0949782cfeda5a119bbfd8c1d98d3e6bec0a
Rob Moore <robmoore121@gmail.com>
2019-05-04 22:25:37 +0100

    Add test for create-page

---
+++ test-create-page.py
@@ -0,0 +1,16 @@
+import subprocess
+import requests
+cmd = "python3 create-page.py test"
+output = list(filter(lambda s: s != '', subprocess.check_output(cmd, shell=True).decode("utf8").split("\n")))
+last_line = output[len(output) - 1]
+url = last_line
+print("Got url " + url)
+response = requests.get(url)
+expected_text = '<!DOCTYPE html>\n<html>\n    <body>\n        <p>Welcome to my toy lambda, which deploys from my machine in a run of a single script! Cool!</p>\n    </body>\n</html>\n'
+assert response.text == expected_text
+print("Pass")
+print("Now deleting page")
+cmd = "python3 delete-page.py test"
+subprocess.check_call(cmd, shell=True)
+print("Done")
+exit(0)
