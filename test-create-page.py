c5e05074d703bec333677e319c072a57d1fea9ec
Rob Moore <robmoore121@gmail.com>
2019-05-08 19:58:02 +0100

    Update test text

--- test-create-page.py
+++ test-create-page.py
@@ -9,7 +9,52 @@ def test_page_creation(page_name):
     url = last_line
     print("\tGot url " + url)
     response = requests.get(url)
-    expected_text = '<!DOCTYPE html>\n<html>\n    <body>\n        <p>Stubbed lambda</p>\n    </body>\n</html>\n'
+    expected_text = """<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Title</title>
+</head>
+
+<body bgcolor="#f9f9f9">
+    <h4 align="center">Date</h4>
+    <h1 align="center">Title</h1>
+    <h3 style="color: orange; margin-left: 30%; margin-right: 30%">Subheading 1</h3>
+    <p style="margin-left: 30%; margin-right: 30%; line-height: 2">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+        standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
+        a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+        remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
+        Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+    </p>
+    <pre
+        style="margin-left: 30%; margin-right: 30%; border-style: solid; border-width: 1px; border-color: black; border-radius: 5px; font-size: 150%; padding: 10px">
+const fs = require('fs')
+
+exports.handler = async (event) => {
+    const document = fs.readFileSync("document.html", "utf8")
+    const response = {
+        statusCode: 200,
+        headers: {
+            "content-type": "text/html"
+        },
+        body: document,
+    }
+    return response
+}</pre>
+    <h3 style="color: orange; margin-left: 30%; margin-right: 30%">Subheading 2</h3>
+    <p style="margin-left: 30%; margin-right: 30%; line-height: 2">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+        standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
+        a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+        remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
+        Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+    </p>
+</body>
+
+</html>"""
     assert response.text == expected_text
     print("\tPass")
     print("\tNow deleting page")
