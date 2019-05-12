2ef691726417c72e0e784b9e28f99da5af112c97
Rob Moore <robmoore121@gmail.com>
2019-05-12 21:18:54 +0100

    Update template lambda to use loaded css script

--- initial-page/src/index.js
+++ initial-page/src/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 
-exports.handler = async (event) => {
+function respondWithDocument(event) {
     const document = fs.readFileSync("document.html", "utf8")
     const response = {
         statusCode: 200,
@@ -10,4 +10,31 @@ exports.handler = async (event) => {
         body: document,
     }
     return response
+}
+
+function respondWithStyles() {
+    const document = fs.readFileSync("document.css", "utf8")
+    const response = {
+        statusCode: 200,
+        headers: {
+            "content-type": "text/css"
+        },
+        body: document,
+    }
+    return response
+}
+
+function queryStringParameter(event, parameter) {
+    if (event.queryStringParameters) {
+        return event.queryStringParameters[parameter]
+    } else {
+        return null
+    }
+}
+
+exports.handler = async (event) => {
+    if (queryStringParameter(event, "resource") == "css") {
+        return respondWithStyles()
+    }
+    return respondWithDocument(event)
 }
--- test-create-page.py
+++ test-create-page.py
@@ -1,6 +1,14 @@
 import subprocess
 import requests
 
+def check_request_content_matches_file(filename, url):
+    response = requests.get(url)
+    expected_text = ""
+    with open(filename, "r") as expected_document:
+        expected_text = expected_document.read()
+    assert response.text == expected_text
+
+
 def test_page_creation(page_name):
     print("Testing page creation for a page called '" + page_name + "'")
     cmd = "python3 create-page.py " + page_name
@@ -8,55 +16,11 @@ def test_page_creation(page_name):
     last_line = output[len(output) - 1]
     url = last_line
     print("\tGot url " + url)
-    response = requests.get(url)
-    expected_text = """<!DOCTYPE html>
-<html>
-
-<head>
-    <title>Title</title>
-</head>
-
-<body bgcolor="#f9f9f9">
-    <h4 align="center">Date</h4>
-    <h1 align="center">Title</h1>
-    <h3 style="color: orange; margin-left: 30%; margin-right: 30%">Subheading 1</h3>
-    <p style="margin-left: 30%; margin-right: 30%; line-height: 2">
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-        standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
-        a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
-        remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
-        Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
-    </p>
-    <pre
-        style="margin-left: 30%; margin-right: 30%; border-style: solid; border-width: 1px; border-color: black; border-radius: 5px; font-size: 150%; padding: 10px">
-const fs = require('fs')
-
-exports.handler = async (event) => {
-    const document = fs.readFileSync("document.html", "utf8")
-    const response = {
-        statusCode: 200,
-        headers: {
-            "content-type": "text/html"
-        },
-        body: document,
-    }
-    return response
-}</pre>
-    <h3 style="color: orange; margin-left: 30%; margin-right: 30%">Subheading 2</h3>
-    <p style="margin-left: 30%; margin-right: 30%; line-height: 2">
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-        standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
-        a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
-        remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
-        Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
-    </p>
-</body>
-
-</html>"""
-    assert response.text == expected_text
-    print("\tPass")
+    print("\tChecking html")
+    check_request_content_matches_file("initial-page/src/document.html", url)
+    print("\tChecking css")
+    check_request_content_matches_file("initial-page/src/document.css", url + "?resource=css")
+    print("\tAll Passed")
     print("\tNow deleting page")
     cmd = "python3 delete-page.py " + page_name
     subprocess.check_call(cmd, shell=True)
